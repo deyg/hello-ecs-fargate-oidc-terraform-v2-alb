ab5f1065883e48493f0cde7dbb1acf104fb61235
deyg <denilson.souza@live.com>
2025-10-19 04:19:39 -0300

    Atualiza output com stage prod

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,3 @@
 output "cluster_name" { value = aws_ecs_cluster.this.name }
 output "ecr_repo"     { value = aws_ecr_repository.app.repository_url }
-output "api_url"      { value = "${aws_apigatewayv2_api.api.api_endpoint}/hello" }
+output "api_url"      { value = "${aws_apigatewayv2_api.api.api_endpoint}/prod/hello" }
