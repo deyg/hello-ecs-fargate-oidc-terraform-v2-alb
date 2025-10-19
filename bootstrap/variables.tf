3dbb83e5e5047d581c97116b8e4d6c0935f9112c
deyg <denilson.souza@live.com>
2025-10-19 03:00:21 -0300

    Define defaults do repo GitHub

--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region"        { type = string  default = "sa-east-1" }
-variable "gh_owner"          { type = string  description = "GitHub org/user" }
-variable "gh_repo"           { type = string  description = "GitHub repository name" }
+variable "gh_owner"          { type = string  default = "deyg"  description = "GitHub org/user" }
+variable "gh_repo"           { type = string  default = "hello-ecs-fargate-oidc-terraform-v2-alb"  description = "GitHub repository name" }
 variable "state_bucket_name" { type = string  default = "tfstate-7t-demo" }
 variable "lock_table_name"   { type = string  default = "tf-locks-7t-demo" }
